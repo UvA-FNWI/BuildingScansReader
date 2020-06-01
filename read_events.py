42157269ed1d30a04e8dafff2bbc1c64baf43a6f
Gerrit Oomens <G.Oomens@uva.nl>
2020-06-01 19:29:11 +0200

    Read last settings from disk

--- read_events.py
+++ read_events.py
@@ -6,7 +6,9 @@ import random
 import os
 import jsonpickle
 import pygame
+import sys
 from datetime import datetime
+from glob import glob
 
 pygame.mixer.init()
 dings = [pygame.mixer.Sound("ding1.wav"), pygame.mixer.Sound("ding2.wav")]
@@ -15,35 +17,45 @@ endpoint = ""
 
 
 class Settings:
-    currentDate = datetime.today()
-    salt = random.randint(0, 2**32)
-    logfolder = f"./logs/{datetime.today().strftime('%Y%m%d')}/"
-
     def __init__(self):
+        self.currentDate = datetime.today().date()
+        self.salt = random.randint(0, 2**32)
+        self.logfolder = f"./logs/{datetime.today().strftime('%Y%m%d')}/"
         if not os.path.exists(self.logfolder):
             os.mkdir(self.logfolder)
         self.employees = set()
         self.students = set()
         self.totalEmployees = set()
         self.totalStudents = set()
+        print(f"Created settings: {self.logfolder}")
 
-settings = Settings()
+try:
+    lastFolder = sorted(glob('./logs/*/'))[-1]
+    lastFile = sorted(os.listdir(lastFolder))[-1]
+    with open(f"{lastFolder}{lastFile}", 'r') as f:
+        settings = jsonpickle.decode(f.read())
+    print(f"Loaded settings ({len(settings.totalStudents)} students, {len(settings.totalEmployees)} employees, {settings.logfolder})")
+    if settings.currentDate != datetime.today().date():
+        settings = Settings()
+except:
+    print(f"Failed to load settings from disk: {sys.exc_info()[0]}")
+    settings = Settings()
 
 def playSound(event):
     dings[event].play()
 
 def handleRead(event, val):
     global settings
-    #if settings.currentDate != datetime.today():
-    #    settings = Settings()
+    if settings.currentDate != datetime.today().date():
+        settings = Settings()
     h = hashlib.sha224(f'{settings.salt}{val}'.encode('utf-8')).hexdigest()
-    isEmployee = val.split(';')[-1].startswith('2') 
-    target = settings.employees if isEmployee else settings.students
+    isStudent = val.split(';')[-1].startswith('1') 
+    target = settings.students if isStudent else settings.employees
     if event == 1:
         target.add(h)
     elif h in target:
         target.remove(h) 
-    totalTarget = settings.totalEmployees if isEmployee else settings.totalStudents
+    totalTarget = settings.totalStudents if isStudent else settings.totalEmployees
     totalTarget.add(h)
     writeData()
 
@@ -62,9 +74,9 @@ def writeData():
         print("Error writing data to API")
     try:
         with open(f"{settings.logfolder}{datetime.now().strftime('%H%M%S%f.json')}", "w") as f:
-            f.write(jsonpickle.encode(settings, unpicklable=False))
+            f.write(jsonpickle.encode(settings))
     except:
-        print("Error writing data to disk")
+        print(f"Error writing data to disk: {sys.exc_info()[0]}")
 
 def readEvents(device):
     dev = InputDevice(f'/dev/input/event{device}')
@@ -85,6 +97,7 @@ def readEvents(device):
                 elif len(code[1]) == 1:
                     val += code[1]
 
+writeData()
 e1 = threading.Thread(target=readEvents, args=(0,),daemon=True)
 e2 = threading.Thread(target=readEvents, args=(1,),daemon=True)
 e1.start()
