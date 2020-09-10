3a3280db29d668be5487fe2233a5ae82f375706c
Sam van Kampen <sam@tehsvk.net>
2020-09-10 21:01:08 +0200

    Add setup script and configuration/service file templates
    This commit adds a network configuration template along with an SSH
    tunneling .service file template and a .service file for the scanner
    script.
    
    Additionally, a setup script is provided which will prompt for some
    information before setting up the configuration based on the templates.
    If the configuration templates change, the setup script can be run again
    to update the configuration.

---
+++ setup_pi.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+HASEDUROAM=$(awk '/eduroam/' /etc/wpa_supplicant/wpa_supplicant.conf)
+
+if [[ -z $HASEDUROAM ]]; then
+    echo -n "Wifi identity (xxx@uva.nl): "
+    read WIFI_IDENTITY
+    echo -n "Wifi password: "
+    read WIFI_PASSWORD
+
+    sudo bash -c "sed 's/IDENTITY/$WIFI_IDENTITY/;s/PASSWORD/$WIFI_PASSWORD/' network_template.conf >>/etc/wpa_supplicant/wpa_supplicant.conf"
+fi
+
+sudo systemctl restart networking.service
+sudo systemctl restart wpa_supplicant.service
+sudo systemctl restart dhcpcd.service
+
+echo "Waiting for a route to become available..."
+while :; do
+	ROUTELINE=$(route -n | awk '/wlan0/ && $2 != "0.0.0.0"')
+	if [[ -n $ROUTELINE ]]; then
+		break;
+	fi
+	sleep 1;
+done
+
+if [[ -e "/etc/systemd/system/ssh-forward.service" ]]; then
+    echo "Updating ssh-forward.service..."
+    NEWPORT=$(awk 'match($0, /([[:alnum:]]+):localhost:22/, fields) { print fields[1]; }' /etc/systemd/system/ssh-forward.service)
+    TUNNEL_ENDPOINT=$(awk 'match($0, /scanner@([a-z.]+)/, fields) { print fields[0]; }' /etc/systemd/system/ssh-forwad.service)
+    sudo bash -c "sed 's/POORT/$NEWPORT/;s/ENDPOINT/$TUNNEL_ENDPOINT/' ssh-forward.service >/etc/systemd/system/ssh-forward.service"
+    sudo systemctl daemon-reload
+    sudo systemctl restart ssh-forward.service
+else
+    # set up ssh
+    mkdir -p ~/.ssh
+    echo "Generating SSH keys..."
+    ssh-keygen -t rsa -f ~/.ssh/id_rsa -q -N ""
+    chmod 700 ~/.ssh
+
+    echo "Go copy the SSH key to the server (starting a shell...)"
+    bash
+
+    echo "Setting up SSH forward service..."
+    echo -n "Tunnel endpoint: "
+    read TUNNEL_ENDPOINT
+
+    scp scanner@$TUNNEL_ENDPOINT:last_port .
+    read PORT <last_port
+    let NEWPORT=($PORT + 1)
+
+    ssh scanner@$TUNNEL_ENDPOINT "echo $NEWPORT >last_port"
+
+    sudo bash -c "sed 's/POORT/$NEWPORT/;s/ENDPOINT/$TUNNEL_ENDPOINT/' ssh-forward.service >/etc/systemd/system/ssh-forward.service"
+    sudo systemctl daemon-reload
+    sudo systemctl enable --now ssh-forward.service
+    echo "Forwarding port $NEWPORT on remote to 22 localhost (sshd)."
+
+    echo "Enabling sshd listening on localhost..."
+    sudo sed -i -e "s/#ListenAddress 0.0.0.0/ListenAddress localhost/" /etc/ssh/sshd_config
+    sudo systemctl enable --now ssh.service
+fi
+
+echo "Installing scanner.service..."
+sudo cp scanner.service /etc/systemd/system
+echo "To enable the scanner service on boot, run 'systemctl enable scanner.service'."
+
+echo -n "Endpoint for scanner data: "
+read SCANNER_ENDPOINT
+
+echo -n "Zone for scanner (C or G): "
+read SCANNER_ZONE
+
+sed -i -e 's/ENDPOINT/$SCANNER_ENDPOINT/;s/ZONE/$SCANNER_ZONE/' read_events.py
