39cf27e79af32c24c390d5abcfa28f8b63233b19
Sam van Kampen <sam@tehsvk.net>
2020-09-10 20:48:04 +0200

    Add multiple location and single-reader support.
    To support multiple locations, some server-side code is now used which
    collects entry and exit notifications from multiple clients. This change
    is reflected in the client by removing the client-side state and
    changing the message format which is sent to the server.
    
    Single-reader support is added, by finding all usb input devices which
    correspond to known readers, and counting their amount. If there are n
    readers, the client will then listen for evdev events from
    /dev/input/event{0..n-1}. The first reader will count as an entry
    reader, the second as an exit reader.
    
    Additionally, pygame exceptions in case no audio devices are found are
    now properly handled (by ignoring them).

--- read_events.py
+++ read_events.py
@@ -9,99 +9,82 @@ import pygame
 import sys
 from datetime import datetime
 from glob import glob
+import traceback
 
-pygame.mixer.init()
-dings = [pygame.mixer.Sound("ding1.wav"), pygame.mixer.Sound("ding2.wav")]
-
-endpoint = ""
-
-
-class Settings:
-    def __init__(self):
-        self.currentDate = datetime.today().date()
-        self.salt = random.randint(0, 2**32)
-        self.logfolder = f"./logs/{datetime.today().strftime('%Y%m%d')}/"
-        if not os.path.exists(self.logfolder):
-            os.mkdir(self.logfolder)
-        self.employees = set()
-        self.students = set()
-        self.totalEmployees = set()
-        self.totalStudents = set()
-        print(f"Created settings: {self.logfolder}")
+dings = []
 
 try:
-    lastFolder = sorted(glob('./logs/*/'))[-1]
-    lastFile = sorted(os.listdir(lastFolder))[-1]
-    with open(f"{lastFolder}{lastFile}", 'r') as f:
-        settings = jsonpickle.decode(f.read())
-    print(f"Loaded settings ({len(settings.totalStudents)} students, {len(settings.totalEmployees)} employees, {settings.logfolder})")
-    if settings.currentDate != datetime.today().date():
-        settings = Settings()
+    pygame.mixer.init()
+    dings = [pygame.mixer.Sound("ding1.wav"), pygame.mixer.Sound("ding2.wav")]
 except:
-    print(f"Failed to load settings from disk: {sys.exc_info()[0]}")
-    settings = Settings()
+    traceback.print_exc()
+
+
+zone = "ZONE" # "G" or "C"
+endpoint = "ENDPOINT"
 
 def playSound(event):
-    dings[event].play()
+    try:
+        dings[event].play()
+    except:
+        traceback.print_exc()
 
-def handleRead(event, val):
-    global settings
-    if settings.currentDate != datetime.today().date():
-        settings = Settings()
-    h = hashlib.sha224(f'{settings.salt}{val}'.encode('utf-8')).hexdigest()
-    isStudent = val.split(';')[-1].startswith('1') 
-    target = settings.students if isStudent else settings.employees
-    if event == 1:
-        target.add(h)
-    elif h in target:
-        target.remove(h) 
-    totalTarget = settings.totalStudents if isStudent else settings.totalEmployees
-    totalTarget.add(h)
-    writeData()
+def handleRead(device, val):
+    hash = hashlib.sha224(f'{val}'.encode('utf-8')).hexdigest()
+    isStudent = val.split(';')[-1].startswith('1')
+    isExit = (device == 1)
 
-def writeData():
+    playSound(1 if isExit else 0) # Play sound
+
+    writeData(hash, isExit, isStudent)
+
+def writeData(hash, isExit, isStudent):
     global settings
     try:
         r = requests.post(endpoint, json={
-            "Date": str(datetime.now()),
-            "Students": len(settings.students),
-            "Employees": len(settings.employees),
-            "TotalStudents": len(settings.totalStudents),
-            "TotalEmployees": len(settings.totalEmployees)
+            "Hash": hash,
+            "IsExit": isExit,
+            "IsStudent": isStudent,
+            "Zone": zone
         })
         print(r)
     except:
-        print("Error writing data to API")
-    try:
-        with open(f"{settings.logfolder}{datetime.now().strftime('%H%M%S%f.json')}", "w") as f:
-            f.write(jsonpickle.encode(settings))
-    except:
-        print(f"Error writing data to disk: {sys.exc_info()[0]}")
+        print("Error writing data to API:")
+        traceback.print_exc()
 
 def readEvents(device):
-    dev = InputDevice(f'/dev/input/event{device}')
+    dev = InputDevice(f"/dev/input/event{device}")
     val = ""
+
     for event in dev.read_loop():
         if event.type == ecodes.EV_KEY:
             data = categorize(event)
             code = data.keycode.split('_')
             if data.keystate == 1:
-                if val == "":
-                    threading.Thread(target=playSound, args=(device,)).start()
+                # Enter has been pressed; this means done reading pass.
                 if code[1] == "ENTER":
                     print(f'{device}: {val}')
-                    threading.Thread(target=handleRead, args=(device, val)).start()                   
+                    threading.Thread(target=handleRead, args=(device, val)).start()
                     val = ""
+                # When semicolon gets detected, it means a substring of the input
+                # is done being parsed and the next substring can be parsed.
                 elif code[1] == "SEMICOLON":
                     val += ";"
+                # Finally, if the character is neither enter nor semicolon,
+                # it is a regular input value character.
                 elif len(code[1]) == 1:
                     val += code[1]
 
-writeData()
-e1 = threading.Thread(target=readEvents, args=(0,),daemon=True)
-e2 = threading.Thread(target=readEvents, args=(1,),daemon=True)
-e1.start()
-e2.start()
 
-e1.join()
-e2.join()
+readerType1 = glob('/dev/input/by-id/*OMNIKEY*')
+readerType2 = glob('/dev/input/by-id/*NEDAP*')
+numReaders = len(readerType1 + readerType2)
+threadList = []
+
+for reader in range(numReaders):
+    newThread = threading.Thread(target=readEvents, args=(reader,),daemon=True)
+    newThread.start()
+    threadList.append(newThread)
+
+for thread in threadList:
+    thread.join()
